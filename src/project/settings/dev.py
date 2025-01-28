83ad7a6f1ded121e49ebb6df69d8269c2ff6beb4
Capa00 <dppgiuseppe@gmail.com>
2025-01-28 22:18:15 +0100

    added django wagtail stylw

---
+++ src/project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(7s*!w@=rs0o-6*g7vf%pwlodsdj04y0q#h)&p6fbd^5$_yj^0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
