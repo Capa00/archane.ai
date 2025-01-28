89cf1b3b312a92b0feb53ad9db606068ce9a4498
Capa00 <dppgiuseppe@gmail.com>
2025-01-28 23:42:46 +0100

    fix postgres env variables

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -71,9 +71,9 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", "archane"),
-        'USER': os.getenv("DB_USER", "django"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "password"),
+        'NAME': os.getenv("DB_NAME", "postgres"),
+        'USER': os.getenv("DB_USER", "postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "postgres"),
         'HOST': os.getenv("DB_HOST", "localhost"),
         'PORT': os.getenv("DB_PORT", "5432"),
     }
